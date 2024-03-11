cc77912c3d709e5baf4ab0e78c501693a98ab541
Alexis DELFORGES <alexis.delforges@sogelink.com>
2024-03-11 08:50:29 +0100

    Class wrapper for rad_to_deg

--- proj_wrapper.cpp
+++ proj_wrapper.cpp
@@ -14,8 +14,32 @@
 extern "C" {
 #endif
 
+class ProjWrapper{
+public :
+    double ToDeg(const double &deg) {
+        return proj_todeg(deg);
+    }
+};
+
 DLL_EXPORT_MACRO double projToDeg(double angleInRad) {
     return proj_todeg(angleInRad);
+}
+
+DLL_EXPORT_MACRO ProjWrapper* CreateNativeObject()
+{
+    ProjWrapper* obj = new ProjWrapper();
+    // you might want to store the object reference on the native side for tracking
+    return obj;
+}
+DLL_EXPORT_MACRO void DestroyNativeObject(ProjWrapper* obj)
+{
+    // may need to update your tracking in native code
+    delete obj;
+}
+
+DLL_EXPORT_MACRO double NativeObjectToDeg(ProjWrapper* obj, double deg)
+{
+    return obj->ToDeg(deg);
 }
 
 #ifdef __cplusplus
